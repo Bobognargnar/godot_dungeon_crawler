5d989a15f13342078f1ca3a629cc437a81132d52
Bobognargnar <fbr.larosa@gmail.com>
2024-10-12 18:10:11 +0200

    feat: update player movement

--- dungeon_crawler_demo/dungeon_demo/player.gd
+++ dungeon_crawler_demo/dungeon_demo/player.gd
@@ -1,33 +1,49 @@
-extends Area2D
+extends CharacterBody2D
 
 signal hit # Signals when the player is being hit
 
 @export var speed = 400 # How fast the player will move (pixels/sec).
+@export var acc = 1000 # How fast the player will move (pixels/sec).
+@export var brake = 2000
 var screen_size # Size of the game window.
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 	hide()
-	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var velocity = Vector2.ZERO # The player's movement vector.
+	#velocity = Vector2.ZERO # The player's movement vector.
+	var delta_v = Vector2.ZERO
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
+		delta_v.x += 1
 	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
+		delta_v.x -= 1
 	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
+		delta_v.y += 1
 	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
+		delta_v.y -= 1
 
+	if delta_v.length() > 0:
+		delta_v = delta_v.normalized() * acc * delta
+		velocity += delta_v
+		# Clamp velocity to max speed
+		if velocity.length() > speed:
+			velocity = velocity.normalized() * speed
+		
+	else:
+		# Clamp velocity to braking
+		if (velocity.length() < brake * delta):
+			velocity = Vector2.ZERO
+		else:
+			velocity -= velocity.normalized() * brake * delta
+		
 	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
+		
 		
 	if velocity.length() != 0:
 		$AnimatedSprite2D.animation = "walk"
@@ -35,10 +51,11 @@ func _process(delta: float) -> void:
 		# See the note below about the following boolean assignment.
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 	
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
+	move_and_slide()
+	#position += velocity * delta
+	#position = position.clamp(Vector2.ZERO, screen_size)
 
 func start(pos):
 	position = pos
 	show()
-	$CollisionShape2D.disabled = false
+	#$CollisionShape2D.disabled = false
