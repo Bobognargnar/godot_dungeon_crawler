f35bc211fda4438eb32267bc63fb09017b9c100d
thelazyone <giacomo.pantalone@gmail.com>
2024-10-16 18:28:01 +0200

    added exc in gitignore, minor cleanup

--- dungeon_crawler_demo/dungeon_demo/main.gd
+++ dungeon_crawler_demo/dungeon_demo/main.gd
@@ -6,13 +6,16 @@ var player_scene = preload("res://creatures/Player.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
+	# Initializing all the creatures to have the player as target.
 	var creatures = []
 	findByClass(self, "CharacterBody2D", creatures)
 	for creature in creatures:
 		if creature.name != "Player":
-			print(creature.name)
+			#print("initializing ", creature.name, "...")
 			creature.target = $Player
-	pass
+			
+	# TODO - maybe loading level0 here rather than having it in the main? 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -20,7 +23,6 @@ func _process(delta: float) -> void:
 
 # Find all nodes of a certain class
 func findByClass(node: Node, className : String, result : Array) -> void:
-	print(node)
 	if node.is_class(className):
 		result.push_back(node)
 	for child in node.get_children():
@@ -30,7 +32,6 @@ func manage_player_health(dam_perc: float) -> void:
 	var hp_left = $Hud.update_health_bar(dam_perc)
 	if hp_left <= 0:
 		game_over()
-	pass
 
 func game_over() -> void:
 	$Hud.show_game_over()
